9becc351acfcdcafc1f0d280a491e3d9ff70ce7c
dineshchidam-commits <dineshchidam@gmail.com>
2026-02-22 11:49:31 +0530

    Create studentname_stage2.py

---
+++ studentname_stage2.py
@@ -0,0 +1,34 @@
+#stage1: basic calculator
+number1 = float(input("Enter The first Number: "))
+number2 =  float(input("Enter The First Number: "))
+operator = input("Enter The Operator(+,-,*,-/): ")
+#addition
+if operator == "+":
+  result = number1+number2
+  print ("Result: ", result)
+  #subtraction
+elif operator == "-":
+  result = number1-number2
+  print ("Result: ", result)
+  #multiplicaton
+elif operator == "*":
+  result = number1*number2
+  print ("Result: ", result)
+  #division
+elif operator == "/":
+  if number2 == 0:
+    print("Error: divided by 0 is allowed")
+  else:
+     result = number1/number2
+     print ("Result: ", result)
+     
+else:
+  print("Invalid Operation")
+
+#Result check
+if result > 0:
+  print ("Positive")
+elif result < 0:
+  print ("positive")
+else:
+  print ("zero No Negetive & No Positive")
